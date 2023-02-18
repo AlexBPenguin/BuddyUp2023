3969976e39e167588dfd80366e8a406940aa03bf
Alex <a.beteta9@gmail.com>
2023-02-17 20:46:34 -0800

    Changed Scene for the First Time

--- Assets/KrakenScript.cs
+++ Assets/KrakenScript.cs
@@ -7,7 +7,7 @@ public class KrakenScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello");
+        Debug.Log("NewBuild!");
     }
 
     // Update is called once per frame
